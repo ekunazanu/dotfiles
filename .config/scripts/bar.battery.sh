4184a0d294733fc7710601fa73ee742de5bb2574
ekunazanu <me@ekunazanu.foo>
2025-01-26 02:29:00 +0530

    feat: i3blocks: highlight battery overcharging
    Highlight battery status to prevent it from charging to over 80%, to improve battery health.

--- .config/scripts/bar.battery.sh
+++ .config/scripts/bar.battery.sh
@@ -4,6 +4,7 @@ BAT=$(cat /sys/class/power_supply/BAT0/capacity)
 STATUS=$(cat /sys/class/power_supply/BAT0/status)
 echo "$BAT%"
 echo "$BAT%"
+[ "$STATUS" = "Charging" ] && [ $BAT -ge 80 ] && exit 33
 [ "$STATUS" = "Charging" ] && echo "#a6cf88" && exit 0
 [ $BAT -le 25 ] && exit 33
 [ $BAT -le 40 ] && echo "#f18e91"
