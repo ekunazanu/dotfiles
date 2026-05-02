4f468b9d46de09842fccbbc3ea6b375cfa3f76ea
ekunazanu <me@ekunazanu.foo>
2026-05-02 20:12:40 +0530

    scripts/passwords: hide passphrase input

--- .config/scripts/tools.passwords.sh
+++ .config/scripts/tools.passwords.sh
@@ -4,7 +4,7 @@ PLATFORM=$(tofi </dev/null --prompt-text "platform:"); if [ -z "$PLATFORM" ]; th
 ACCOUNT=$(tofi </dev/null --prompt-text "account:");  if [ -z "$ACCOUNT" ]; then exit; fi
 SEED=$(echo -e "0\n1\n2\n3\n4" | tofi --prompt-text "seed:"); if [ -z "$SEED" ]; then exit; fi
 LENGTH=$(echo -e "24\n20\n12" | tofi --prompt-text "length:"); if [ -z "$LENGTH" ]; then exit; fi
-PASSPHRASE=$(tofi </dev/null --prompt-text "passphrase:"); if [ -z "$PASSPHRASE" ]; then exit; fi
+PASSPHRASE=$(tofi </dev/null --hide-input true --hidden-character "*" --prompt-text "passphrase:"); if [ -z "$PASSPHRASE" ]; then exit; fi
 
 PASSWORD=$(echo -n "$ACCOUNT$PLATFORM$SEED$PASSPHRASE" | sha256sum | awk '{print $1}' | xxd -r -p | base64 | cut -c -$LENGTH)
 
