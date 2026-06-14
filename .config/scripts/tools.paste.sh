8015503a63c995c1273d35a811a205f562bb73e1
ekunazanu <me@ekunazanu.foo>
2026-06-14 18:43:17 +0530

    scripts/paste: fix path and if condition syntax
    Shell expects spaces around brackets. Also remove whitespaces from path, and use home directory.

--- .config/scripts/tools.paste.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-FILENAME=$(tofi < /dev/null --prompt-text "filename:")
-
-if [-n "$FILENAME"]; then
-    wl-paste > $FILENAME && notify-send -u normal -a "Clipboard" "Pasted file" "Pasted clipboard contents to ~/$FILENAME"
-else
-   notify-send -u normal -a "Clipboard" "Error" "No file name provided" 
-fi
