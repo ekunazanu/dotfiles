e2713ebe49c155e3e57d7e9d05df2c284036a7af
ekunazanu <me@ekunazanu.foo>
2026-09-10 12:22:52 +0530

    scripts/bar/battery: change low battery warning level
    Keep battery within the optimal 30% to 60% range.

--- .config/scripts/bar.battery.sh
+++ .config/scripts/bar.battery.sh
@@ -6,6 +6,6 @@ echo "$BAT%"
 echo "$BAT%"
 [ "$STATUS" = "Charging" ] && [ $BAT -ge 80 ] && exit 33
 [ "$STATUS" = "Charging" ] && echo "#a6cf88" && exit 0
-[ $BAT -le 25 ] && exit 33
+[ $BAT -le 30 ] && exit 33
 [ $BAT -le 40 ] && echo "#f18e91"
 exit 0
